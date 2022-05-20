85f62bbb0da7ed287ca55dcb14e5d391b0fb85e5
Leonardo Mattos <leonardosousamattos@gmail.com>
2022-05-20 15:47:58 -0300

    Treinamento

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <meta lang="pt-br">
+        <link rel="stylesheet" href="style.css">
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </head>
+    <body>
+    <section class="list_course">
+        <div class="container">
+            <div class="block-title">
+                <div class="row align-items-center">
+                    <div class="col-lg-2 col-icon-infos">
+                        <img src="image/selo.svg" class="icon-infos-certifier icon-laranja title" alt="Selo MEC">
+                    </div>
+                    <div class="col-lg-10">
+                        <h3>Cursos reconhecidos pelo MEC pela Portaria nº 1.065, de 18 de outubro de 2018.</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-card-course ">
+                    <a href="#box-form" class="link-courses" data-course-city="" data-course-name="ADMINISTRAÇÃO">
+                        <div class="box-card-course" style="background-image: linear-gradient(to bottom, rgb(0 0 0 / 59%), rgb(0 0 0 / 59%)), url('/wp-content/themes/vestibular/assets/img/course/Administracao.webp'); background-size: cover;" title="ADMINISTRAÇÃO">
+                            <h3 class="card-title title-course-area">ADMINISTRAÇÃO</h3>
+                            <h5 class="card-title sub-title-course-area">Duração: 8 semestres </h5>
+                            <h5 class="card-title sub-title-course-area">Grau: bacharelado</h5>
+                            <div class="box-btn-course">
+                                <button class="btn-course">inscreva-se</button>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    </body>
+</html>
