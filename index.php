3e5e11eef6db26eedf5513513fa3da7794e10fc0
Leonardo Mattos <leonardosousamattos@gmail.com>
2022-05-23 12:31:34 -0300

    Criaçao de Blocos

--- index.php
+++ index.php
@@ -12,27 +12,31 @@
         <div class="container">
             <div class="block-title">
                 <div class="row align-items-center">
-                    <div class="col-lg-2 col-icon-infos">
-                        <img src="image/selo.svg" class="icon-infos-certifier icon-laranja title" alt="Selo MEC">
-                    </div>
-                    <div class="col-lg-10">
+                    <div class="col" style="display: flex; align-items: center; justify-content: space-evenly; align-content: space-between;">
+                        <img src="image/selo.svg" alt="Selo MEC">
                         <h3>Cursos reconhecidos pelo MEC pela Portaria nº 1.065, de 18 de outubro de 2018.</h3>
-                    </div>
                 </div>
             </div>
             <div class="row">
+                <?php
+                $i = 1;
+                while ($i <= 12):
+                ?>
                 <div class="col-lg-3 col-md-6 col-card-course ">
-                    <a href="#box-form" class="link-courses" data-course-city="" data-course-name="ADMINISTRAÇÃO">
-                        <div class="box-card-course" style="background-image: linear-gradient(to bottom, rgb(0 0 0 / 59%), rgb(0 0 0 / 59%)), url('https://vestibular.faculdadesprominas.com.br/wp-content/themes/vestibular/assets/img/course/Administracao.webp'); background-size: cover;" title="ADMINISTRAÇÃO">
-                            <h3 class="card-title title-course-area">ADMINISTRAÇÃO</h3>
-                            <h5 class="card-title sub-title-course-area">Duração: 8 semestres </h5>
-                            <h5 class="card-title sub-title-course-area">Grau: bacharelado</h5>
+                    <a href="#box-form" class="link-courses">
+                        <div class="box-card-course" style="background-image: linear-gradient(to bottom, rgb(0 0 0 / 59%), rgb(0 0 0 / 59%)), url('image/educação.png'); background-size: cover;" title="ADMINISTRAÇÃO">
+                            <h3 class="card-title title-course-area">EDUCAÇÃO</h3>
+                            <h5 class="card-title sub-title-course-area">207 cursos disponiveis </h5>
                             <div class="box-btn-course">
-                                <button class="btn-course">inscreva-se</button>
+                                <span class="btn-course">Ver todos</span>
                             </div>
                         </div>
                     </a>
                 </div>
+                <?php
+                    $i++;
+                endwhile;
+                ?>
             </div>
         </div>
     </section>
